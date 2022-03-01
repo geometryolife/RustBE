69dfb3bc5a2c2989d29aced25524cf2c8d5e0dd8
Joe Chen <geometryolife@gmail.com>
2022-03-01 21:53:21 +0800

    add a general solution for move-zeroes

---
+++ Chapter12/e1_move_zeroes/src/main.rs
@@ -0,0 +1,55 @@
+// 【零移动】
+
+// 【题目描述】
+// 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，
+// 同时保持非零元素的相对顺序。
+
+// 【说明】
+// （1）必须在原数组上操作，不能复制额外的数组。
+// （2）尽量减少操作次数。
+
+// 【示例】
+// 输入：nums = [0,1,0,3,12]
+// 输出：[1,3,12,0,0]
+// 输入：nums = [0]
+// 输出：[0]
+
+// 【链接】https://leetcode-cn.com/problems/move-zeroes
+
+// 【解题思路】
+// 遍历数组 nums，把非零元素（假设有 i 个）按顺序存入数组第 0～i-1 个位置，
+// 再把数组的第 i～n-i 个位置全部设为 0。
+
+struct Solution;
+
+impl Solution {
+    pub fn move_zeroes(nums: &mut Vec<i32>) {
+        let mut i = 0;
+        let mut j = 0;
+
+        // 遍历检查是否为 0，若非零，则往前移
+        while j < nums.len() {
+            if nums[j] != 0 {
+                nums[i] = nums[j];
+                i += 1;
+            }
+            j += 1;
+        }
+
+        let mut k = i;
+
+        // 将下标为 i～n-1 的元素设置为 0
+        while k < nums.len() {
+            nums[k] = 0;
+            k += 1;
+        }
+    }
+}
+
+fn main() {
+    let mut vec: Vec<i32> = vec![0, 1, 0, 3, 12];
+    println!("{:?}", vec);
+
+    Solution::move_zeroes(&mut vec);
+    println!("{:?}", vec);
+}
